cfb71ba1c06476176fb01bad73acd35569e6a95a
BeDaShao <10846015@ntub.edu.tw>
2022-01-24 23:38:25 +0800

    add admin

--- links/admin.py
+++ links/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+
+from links.models import Link
+
 # Register your models here.
+
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ['code', 'url']
+    search_fields = ['code', 'url']
+    list_filter = ['code']
+    
+admin.site.register(Link, LinkAdmin)
